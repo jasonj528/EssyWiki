1a0cbdd074006bb329d862d1a9ee1838b97be675
Jason J <jzephyr528@gmail.com>
2017-12-10 21:27:26 -0500

    Added bootstrap theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 from recommonmark.parser import CommonMarkParser
+import sphinx_bootstrap_theme
 
 source_parsers = {
     '.md': CommonMarkParser,
@@ -88,7 +89,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
