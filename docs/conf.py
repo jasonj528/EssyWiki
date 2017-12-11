ddcd33646b4cbd375c763bd2f195551622b7fc96
Jason J <jzephyr528@gmail.com>
2017-12-10 21:29:38 -0500

    Changed to RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 from recommonmark.parser import CommonMarkParser
-import sphinx_bootstrap_theme
+import sphinx_rtd_theme
 
 source_parsers = {
     '.md': CommonMarkParser,
@@ -89,8 +89,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = sphinx_rtd_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
